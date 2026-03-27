9ecb3c7eaff68ec9365119ec1e02f5ef94997a08
DHEERAJ <sheero1238@gmail.com>
2026-03-28 00:02:31 +0530

    Delete util directory

--- util/model.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-class Darknet:
-    def __init__(self):
-        self.model = torch.hub.load('ultralytics/yolov5','yolov5s',pretrained=True)
-
-    def detect(self, image):
-        results = self.model(image)
-        return results
--- util/parser.py
+++
@@ -1,4 +0,0 @@
-def load_classes(path):
-    with open(path, "r") as f:
-        names = f.read().split("\n")
-    return names
